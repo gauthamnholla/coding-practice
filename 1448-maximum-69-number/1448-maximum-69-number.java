523e843f9e2e746dd0871935f22be3b2091d1ffc
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2025-08-16 10:14:39 +0530

    Time: 0 ms (100%), Space: 40.4 MB (72.77%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int maximum69Number (int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break; // only change the first one found
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+}
