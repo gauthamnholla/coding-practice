240a78216d0fa0c6dc9ee271bb196c05ba2cbb80
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2025-11-19 08:43:30 +0530

    Time: 9 ms (96.45%), Space: 46.2 MB (21.56%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c == '-') continue;
+            sb.append(Character.toUpperCase(c));
+            count++;
+            if (count == k) {
+                sb.append('-');
+                count = 0;
+            }
+        }
+
+        // remove trailing dash if any
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '-') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        return sb.reverse().toString();
+    }
+}
