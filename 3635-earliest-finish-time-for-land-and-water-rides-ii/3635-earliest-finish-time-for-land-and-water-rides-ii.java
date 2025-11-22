e9cc9aec8fa9f8bfc9f2c862b37c4103d7fc0e0d
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2025-11-22 09:46:09 +0530

    Time: 2 ms (100%), Space: 82.8 MB (15.57%) - LeetHub

---
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int earliestFinishTime(int[] a, int[] b, int[] c, int[] d) {
+        int ans = Integer.MAX_VALUE;
+        // take land first
+
+        int n = a.length;
+        int minEnd = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            minEnd = Math.min(minEnd, a[i] + b[i]);
+        }
+        int m = c.length;
+
+        for (int i = 0; i < m; i++) {
+            ans = Math.min(ans, d[i] + Math.max(minEnd, c[i]));
+        }
+
+        // take water first
+        minEnd = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) {
+            minEnd = Math.min(minEnd, c[i] + d[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans = Math.min(ans, b[i] + Math.max(minEnd, a[i]));
+        }
+
+        return ans;
+    }
+}
