759be820c8e804c3bfcbb0a2ff020c9329bdfeae
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2025-11-25 08:46:39 +0530

    Time: 1 ms (100%), Space: 110.3 MB (26%) - LeetHub

---
+++ 3644-maximum-k-to-sort-a-permutation/3644-maximum-k-to-sort-a-permutation.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sortPermutation(int[] nums) {
+        int mask = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) mask &= nums[i];
+        }
+        return mask == Integer.MAX_VALUE ? 0 : mask;
+    }
+}
