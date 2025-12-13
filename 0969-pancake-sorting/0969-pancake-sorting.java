1944137e50dc7ca176636dc72731d487cd897eb2
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2025-12-13 09:33:36 +0530

    Time: 3 ms (6.17%), Space: 45.2 MB (5.4%) - LeetHub

---
+++ 0969-pancake-sorting/0969-pancake-sorting.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> pancakeSort(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int n = arr.length;
+        int[] indices = new int[n];
+        for(int i=0; i<n; ++i) indices[arr[i]-1] = i;
+        for(int i=0; i<n; ++i) {
+            int pull = indices[i];
+            dew(stack, i, pull);
+            indices[arr[i]-1] = pull;
+            indices[i] = i;
+            arr[pull] = arr[i];
+            arr[i] = i+1;
+        }
+        LinkedList<Integer> ans = new LinkedList<>();
+        while(!stack.isEmpty()) {
+            int x = stack.pop();
+            ans.addFirst(x);
+        }
+        return ans;
+    }
+    
+    private void dew(Stack<Integer> stack, int j, int i) {
+        if(i != j) {
+            add(stack, i+1);
+            add(stack, i-j+1);
+            add(stack, i-j);
+            add(stack, i-j-1);
+            add(stack, i-j);
+            add(stack, i+1);
+        }
+    }
+    
+    private void add(Stack<Integer> stack, int x) {
+        if(x > 1) {
+            if(!stack.isEmpty() && stack.peek() == x) stack.pop();
+            else stack.push(x);
+        }
+    }
+}
