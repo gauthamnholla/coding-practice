0331437cba42e720f51b196636cb5e7c4f40465d
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-01-07 10:49:30 +0530

    Time: 0 ms (100%), Space: 42.5 MB (7.34%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int reachNumber(int target) {
+//         // target = Math.abs(target); // We only care about the absolute value
+//         // int sum = 0;
+//         // int steps = 0;
+
+//         // while (sum < target || (sum - target) % 2 != 0) {
+//         //     steps++;
+//         //     sum += steps;
+//         // }
+
+//         // return steps;
+
+        // Optimised approach
+
+        target = Math.abs(target);
+        int start = 0, end = target; 
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            long sum = ((long) mid * (mid + 1)) / 2; 
+            if (sum < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        int n = end;
+        long sum = ((long) n * (n + 1)) / 2;
+        
+        while ((sum - target) % 2 != 0) {
+            n++;
+            sum += n;
+        }
+        
+        return n;
+    }
+}
