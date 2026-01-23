aa5ebfc42243188eca1e23d159e60a557e21d089
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-01-23 08:36:10 +0530

    Time: 8 ms (73.91%), Space: 47.2 MB (27.95%) - LeetHub

---
+++ 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class FrontMiddleBackQueue {
+
+    Deque<Integer> left;
+    Deque<Integer> right;
+
+    public FrontMiddleBackQueue() {
+        left = new ArrayDeque<>();
+        right = new ArrayDeque<>();
+    }
+
+    private void balance() {
+        if (left.size() > right.size() + 1) {
+            right.addFirst(left.removeLast());
+        } else if (left.size() < right.size()) {
+            left.addLast(right.removeFirst());
+        }
+    }
+
+    public void pushFront(int val) {
+        left.addFirst(val);
+        balance();
+    }
+
+    public void pushMiddle(int val) {
+        if (left.size() > right.size()) {
+            right.addFirst(left.removeLast());
+        }
+        left.addLast(val);
+        balance();
+    }
+
+    public void pushBack(int val) {
+        right.addLast(val);
+        balance();
+    }
+
+    public int popFront() {
+        if (left.isEmpty()) return -1;
+
+        int val = left.removeFirst();
+        balance();
+        return val;
+    }
+
+    public int popMiddle() {
+        if (left.isEmpty()) return -1;
+
+        int val = left.removeLast();  // this is the middle
+        balance();
+        return val;
+    }
+
+    public int popBack() {
+        if (left.isEmpty()) return -1;
+
+        int val;
+        if (!right.isEmpty()) {
+            val = right.removeLast();
+        } else {
+            val = left.removeLast();
+        }
+        balance();
+        return val;
+    }
+}
