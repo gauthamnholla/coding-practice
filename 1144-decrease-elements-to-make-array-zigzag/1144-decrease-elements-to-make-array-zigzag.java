56e91fdaf7d5d20fe801edf0b1349758635677e2
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-01-24 09:41:40 +0530

    Time: 0 ms (100%), Space: 43 MB (27.85%) - LeetHub

---
+++ 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int movesToMakeZigzag(int[] nums) {
+       int odd = 0, even =0;
+       int n = nums.length ;
+
+       for(int i=0 ; i< n ; i++){
+        if((i-1>=0 && nums[i-1]<= nums[i]) || (i+1<n && nums[i+1]<= nums[i])){
+            int min = Integer.MAX_VALUE ;
+
+            if(i-1>=0){
+                min = Math.min(min, nums[i-1]);
+            }
+            if(i+1<n){
+            min = Math.min(min , nums[i+1]);
+            }
+           int cost = nums[i]-min+1;
+            if(i %2 == 0){
+                even += cost;
+            }else{
+                odd += cost;
+            }
+        }
+       }
+
+       return Math.min(even, odd); 
+    }
+}
