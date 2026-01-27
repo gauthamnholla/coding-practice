34f7c48ea438758ae241bfe990db0e54c01bc93e
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-01-27 08:23:47 +0530

    Time: 1 ms (100%), Space: 88.3 MB (9.71%) - LeetHub

---
+++ 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        if(sum==goal){
+            return 0;
+        }
+        long diff=Math.abs(goal-sum);
+        if(diff%limit==0){
+            return (int)(diff/(long)limit);
+        }
+        else{
+            return (int)((diff/(long)limit)+1);
+        }
+    }
+}
