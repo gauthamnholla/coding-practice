2b1d6b7d267dfaf6d67acc2f00b8cab02e42376f
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-01-28 09:29:01 +0530

    Time: 1 ms (96.34%), Space: 45.5 MB (17.91%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Apply operations
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        // Step 2: Shift zeros to the end
+        int[] result = new int[n];
+        int index = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+    }
+}
