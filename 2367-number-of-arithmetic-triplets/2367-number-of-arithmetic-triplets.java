7b8904b8559e8762e67a5384856220272d5e5603
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-01-28 09:26:34 +0530

    Time: 1 ms (95.05%), Space: 43.4 MB (38.21%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -0,0 +1,26 @@
+class Solution {
+    private boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int counter = 0;
+        for (int num : nums) {
+            if (binarySearch(nums, num + diff) && binarySearch(nums, num + 2 * diff)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
