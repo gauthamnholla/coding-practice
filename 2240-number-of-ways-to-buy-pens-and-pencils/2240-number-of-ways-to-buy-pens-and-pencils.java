8f73058eadd7d843cf3ddfca2f7e6763d41b66c0
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-02-02 10:21:15 +0530

    Time: 29 ms (6.21%), Space: 42.4 MB (55.28%) - LeetHub

---
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public long waysToBuyPensPencils(int total, int cost1, int cost2)
+    {
+        long ans=0;
+        int val=0;
+        for(int x=0;;x++)
+        {
+            val=total-cost1*x;
+            if(val < 0) break;
+            ans+= (long)Math.floor((long)val /(long) cost2)+1;   
+        }
+         return ans;
+    }
+}
