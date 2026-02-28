19ed3055148a0c085f31f65f6480909c22a6e88b
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-02-28 17:06:01 +0300

    Time: 28 ms (70.65%), Space: 104.7 MB (49.7%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,28 +1,21 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
         int n = nums.length;
-        // dp[i][0] is for first i + 1 elements, the last element is the even element
-        // dp[i][1] is for first i + 1 elements, the last element is the odd element
-        // (the last element may or may not be nums[i])
-        long[][] dp = new long[n][2];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = Long.MIN_VALUE;
-            dp[i][1] = Long.MIN_VALUE;
-        }
-        dp[0][0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            // not use nums[i], or nums[i] as the only element
-            dp[i][0] = Math.max(nums[i], dp[i - 1][0]);
-            if (dp[i - 1][1] != Long.MIN_VALUE) { // or use nums[i]
-                dp[i][0] = Math.max(dp[i][0], dp[i - 1][1] + nums[i]);
-            }
+        //Bottom Up Approach..
+        long [][]dp = new long[n][2];
+        //here at every index we are storing the max sum for even and odd length combo
+        //0 - even 
+        //1 -odd
 
-            // not use nums[i]
-            dp[i][1] = Math.max(dp[i][1], dp[i - 1][1]);
-            if (dp[i - 1][0] != Long.MIN_VALUE) { // or use nums[i]
-                dp[i][1] = Math.max(dp[i][1], dp[i - 1][0] - nums[i]);
-            }
+        dp[0][0]=0;//1st element is at 0 i.e this is at the odd place
+        dp[0][1]=nums[0];//1st element is at 0 i.e this is at the odd place
+        
+        for(int i=1;i<n;i++){
+            int sq= nums[i];
+            dp[i][0]= Math.max(dp[i-1][1]-sq , dp[i-1][0]); //for even pos
+            //
+            dp[i][1]= Math.max(dp[i-1][0]+sq , dp[i-1][1]); //for odd pos
         }
-        return Math.max(dp[n - 1][0], dp[n - 1][1]);
+        return Math.max(dp[n-1][0],dp[n-1][1]);
     }
 }
