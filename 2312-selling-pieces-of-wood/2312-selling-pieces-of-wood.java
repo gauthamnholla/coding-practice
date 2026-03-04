bdbde73099d2f5371ca59eaa3dacc3c6e3e33f30
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-03-04 13:36:45 +0300

    Time: 32 ms (86.05%), Space: 55 MB (74.42%) - LeetHub

---
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.java
@@ -0,0 +1,16 @@
+class Solution {
+        public long sellingWood(int m, int n, int[][] prices) {
+        long[][] dp = new long[m + 1][n + 1];
+        for (int[] p : prices)
+            dp[p[0]][p[1]] = p[2];
+        for (int w = 1; w <= m; ++w) {
+            for (int h = 1; h <= n; ++h) {
+                for (int a = 1; a <= w / 2; ++a)
+                    dp[w][h] = Math.max(dp[w][h], dp[a][h] + dp[w - a][h]);
+                for (int a = 1; a <= h / 2; ++a)
+                    dp[w][h] = Math.max(dp[w][h], dp[w][a] + dp[w][h - a]);
+            }
+        }
+        return dp[m][n];
+    }
+}
