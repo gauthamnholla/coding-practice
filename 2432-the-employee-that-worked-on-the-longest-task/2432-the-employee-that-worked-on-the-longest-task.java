a367471a5ef1af95cbbebf8e897f21ed3e421ece
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-03-05 13:36:36 +0300

    Time: 1 ms (100%), Space: 46.8 MB (93.75%) - LeetHub

---
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int hardestWorker(int n, int[][] logs) {
+        int longestTime = logs[0][1];
+        int id = logs[0][0];
+
+        for(int i = 1; i < logs.length; i++){
+            int time = logs[i][1] - logs[i-1][1];
+            if(time > longestTime){
+                longestTime = time;
+                id = logs[i][0];
+            }
+            if(time == longestTime){
+                id = Math.min(id,logs[i][0]);
+            }
+        }
+        return id;
+    }
+}
