62cb552b5ffa2c83936148258b699d3b6d14eff8
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-03-27 15:26:14 +0300

    Time: 4 ms (84.66%), Space: 136.9 MB (80.73%) - LeetHub

---
+++ 3858-minimum-bitwise-or-from-grid/3858-minimum-bitwise-or-from-grid.java
@@ -0,0 +1,23 @@
+class Solution {
+        public int minimumOR(int[][] grid) {
+        int res = 0;
+        for (int bi = 20; bi >= 0; --bi) {
+            int b = 1 << bi;
+            int mask = res | (b - 1);
+            for (int[] r : grid) {
+                boolean row_all_bad = true;
+                for (int a : r) {
+                    if ((a & mask) == a) {
+                        row_all_bad = false;
+                        break;
+                    }
+                }
+                if (row_all_bad) {
+                    res |= b;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
