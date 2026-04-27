d9a37ed13f76712c6238763446a49f5f16651cfa
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-04-27 15:10:31 +0300

    Time: 1 ms (100%), Space: 47.1 MB (33.97%) - LeetHub

---
+++ 3402-minimum-operations-to-make-columns-strictly-increasing/3402-minimum-operations-to-make-columns-strictly-increasing.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumOperations(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int operations = 0;
+
+        for (int col = 0; col < n; col++) {
+            for (int row = 1; row < m; row++) {
+                if (grid[row][col] <= grid[row - 1][col]) {
+                    operations += grid[row - 1][col] - grid[row][col] + 1;
+                    grid[row][col] = grid[row - 1][col] + 1;
+                }
+            }
+        }
+        return operations;
+    }
+}
